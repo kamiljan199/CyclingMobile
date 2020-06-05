e980edcca750b3b5345b89152ba197dd187387d4
kamiljan199 <kamiljan199@gmail.com>
2020-06-05 03:03:53 +0200

    Pierwsza wersja prototypu. Proste jeżdzenie. Prosta mapa. Kamera podąża za graczem.

---
+++ CyclingMobile/Assets/Scripts/BicycleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BicycleController : MonoBehaviour
+{
+
+    public Rigidbody2D backWheel;
+    public Rigidbody2D frontWheel;
+    public Rigidbody2D bike;
+
+
+    public float bikeTorque = 20;
+    public float speed = 100;
+    public float force = 100;
+
+    private float movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if(force > 0)
+        {
+            backWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            bike.AddTorque(-movement * bikeTorque * Time.fixedDeltaTime);
+        }
+       
+
+    }
+}
