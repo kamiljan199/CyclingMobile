4641508a00a6831267de847f88dbb956219fa097
Bromonov <pawel.ryczkowski12@gmail.com>
2020-06-05 15:32:24 +0200

    Added energy bar + working button for muscle force

---
+++ CyclingMobile/Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool pressed = false;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+    void Update()
+    {
+        if (pressed)
+            GetButtonState();
+    }
+
+    public bool GetButtonState()
+    {
+        return pressed;
+    }
+}
