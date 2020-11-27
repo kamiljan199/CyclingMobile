5b9ba9b585d903d96c697946ae1836fcf51c11f5
TramwajPL <56363425+TramwajPL@users.noreply.github.com>
2020-11-27 15:20:48 +0100

    more cleanup

--- CyclingMobile/Assets/Scripts/Mechanics/BicycleController.cs
+++ CyclingMobile/Assets/Scripts/Mechanics/BicycleController.cs
@@ -51,7 +51,6 @@ public class BicycleController : MonoBehaviour
     public GameObject player;
     public GameObject flag;
 
-    // Start is called before the first frame update
     void Start()
     {
         levelBeaten = GameObject.Find("LevelBeaten");
@@ -65,12 +64,9 @@ public class BicycleController : MonoBehaviour
         LoadSave();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log("goldzik: " + player.GetComponent<Player>().gold);
-
-        //Debug.Log("jak szybko zapierdalasz kolarzu?" + velocity);
+        
         if (animator.GetBool("pressed") == true)
         {
             playanim++;
@@ -86,47 +82,21 @@ public class BicycleController : MonoBehaviour
 
         isButtonPressed = button.GetButtonState();
 
-        //if (currentBoost > 0.0f)
-        //{
-        //    currentBoost -= 0.01f;
-        //}
-        //else if (currentBoost < 0.0f)
-        //{
-        //    currentBoost = 0.0f;
-        //}
-
-        //if(Time.time < boostTime)
-        //{
-        //    currentBoost = maxBoost;
-        //}
-        //else if(Time.time < afterBoostTime)
-        //{
-        //    currentBoost = -maxBoost;
-        //}
-        //else
-        //{
-        //    currentBoost = 0;
-        //}
-
-        //Debug.Log("no dumny ty jestes z siebie czlowieku: " + currentBoost);
+        
         if ( boostClicked == true)
         {
             Debug.Log("BOOOST");
 
             currentBoost = maxBoost;
             boostTime = Time.time + boostTimeDuration;
-            //afterBoostTime = Time.time + boostTimeDuration + afterBoostTimeDuration;
             boostParticle.SetActive(true);
-            //boostParticle.GetComponent<BoostParticle>().StartParticles();
 
-            //boost = false;
             boostClicked = false;
             AddEnergy(-2.5f);
         }
         else if(Time.time > boostTime)
         {
             boostParticle.SetActive(false);
-            //boostParticle.GetComponent<BoostParticle>().StopParticles();
         }
 
         if (velocity < 250.0f)
@@ -135,15 +105,8 @@ public class BicycleController : MonoBehaviour
             {
 
             }
-            //else if ((isButtonPressed || Input.GetKey("right")) && energy > 0)
             else if (energy > 0)
             {
-                //if (movement < 1.0f)
-                //{
-                //    movement += 0.5f * gear * 0.5f;
-                //}
-                //else movement = 1.0f * gear * 0.5f;
-
                 movement = GetMovement() + currentBoost;
             }
             else if (Input.GetKey("left"))
@@ -154,11 +117,6 @@ public class BicycleController : MonoBehaviour
                 }
                 else movement = -1.0f * gear * 0.5f;
             }
-            //else
-            //{
-            //movement = 0.0f;
-            //energy += 0.2f;
-            //}
 
         }
         else if(velocity > LevelInformations.maxSpeed)
@@ -171,7 +129,6 @@ public class BicycleController : MonoBehaviour
             movement = 0.0f;
         }
 
-        //recovering energy when running downwards
         if (bike.transform.rotation.z < 0.0f && bike.transform.rotation.z > -90.0f)
         {
             AddEnergy(0.005f);
@@ -180,18 +137,14 @@ public class BicycleController : MonoBehaviour
         currentBoost = 0.0f;
         energyBar.SetEnergy(energy);
 
-        //Debug.Log(GetVelocity());
     }
 
     private void FixedUpdate()
     {
         if (force > 0)
         {
-            //Debug.Log(movement);
-
             backWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
             frontWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
-            //bike.AddTorque(movement * bikeTorque * Time.fixedDeltaTime);
         }
     }
 
@@ -208,81 +161,54 @@ public class BicycleController : MonoBehaviour
         {
             if(GetVelocity() <= 100.0f)
             {
-                //AddEnergy(-0.1f);
-                //velocity change 0.0 -> 0.(3)
                 x = GetVelocity() / 300.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.3f;
             }
             else if (GetVelocity() <= 200.0f && GetVelocity() > 100.0f)
             {
-                //AddEnergy(-0.05f);
-                //velocity change 0.3 -> 0.1
                 x = 0.5f - (GetVelocity()) / 1000.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.05f;
             }
             else 
             {
-                //AddEnergy(-0.01f);
-                //velocity change 0.1005 -> 0.005
                 x = 0.3005f - (GetVelocity()) / 1000.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.005f;
             }
         }
         else if(gear == 2)
         {
             if (GetVelocity() <= 100.0f)
             {
-                //AddEnergy(-0.2f);
-                //velocity change 0.0 -> 0.15
                 x = GetVelocity()*3 / 3000.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.1f;
             }
             else if (GetVelocity() <= 200.0f && GetVelocity() > 100.0f)
             {
-                //AddEnergy(-0.1f);
-                //velocity change 0.16.67 -> 0.204
                 x = 0.13f + GetVelocity() / 2700.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.2f;
             }
             else
             {
-                //AddEnergy(-0.05f);
-                //velocity change 0.2025 -> 0.0025
                 x = 0.6025f - (GetVelocity()) / 5500.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.005f;
             }
         }
         else
         {
             if (GetVelocity() <= 100.0f)
             {
-                //AddEnergy(-0.5f);
-                //velocity change 0.0 -> 0.05
                 x = GetVelocity() / 2000.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.05f;
             }
             else if (GetVelocity() <= 200.0f && GetVelocity() > 100.0f)
             {
-                //AddEnergy(-0.2f);
-                //velocity change 0.05 -> 0.1
                 x = GetVelocity() / 2000.0f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.1f;
             }
             else
             {
-                //AddEnergy(-0.05f);
-                //velocity change 0.1 -> 0.2
                 x = GetVelocity() / 1000.0f - 0.1f;
                 return x * LevelInformations.gearUpgrade;
-                //return 0.2f;
             }
         }
     }
@@ -360,7 +286,6 @@ public class BicycleController : MonoBehaviour
 
             SaveSystem.SavePlayer(player.GetComponent<Player>());
             
-            //Application.Quit();
             SceneManager.LoadScene("LevelChoser", LoadSceneMode.Single);
         }
     }
