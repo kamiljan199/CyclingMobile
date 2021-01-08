a9ecc50bc2478470322e0a1ec83a0e5755b1447d
Bromonov <pawel.ryczkowski12@gmail.com>
2021-01-08 17:57:48 +0100

    quick fix with directive problem in tutorial script

--- CyclingMobile/Assets/Scripts/Tutorial.cs
+++ CyclingMobile/Assets/Scripts/Tutorial.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
