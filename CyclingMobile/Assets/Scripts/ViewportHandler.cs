f72fe9f72e0ba7dec5863d3ad51381e42d217429
Bromonov <pawel.ryczkowski12@gmail.com>
2021-01-21 22:51:36 +0100

    fixed ui scaling problem and handling different screen resolutions

---
+++ CyclingMobile/Assets/Scripts/ViewportHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ViewportHandler : MonoBehaviour
+{
+    private float screenwidth;
+    private float screenheight;
+
+    Camera camera;
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        screenwidth = Screen.width;
+        screenheight = Screen.height;
+
+        camera.orthographicSize = 20 / screenwidth * screenheight / 2;
+
+    }
+
+    // Adjust the camera's height so the desired scene width fits in view
+    // even if the screen/window size changes dynamically.
+    void Update()
+    {
+        
+    }
+}
