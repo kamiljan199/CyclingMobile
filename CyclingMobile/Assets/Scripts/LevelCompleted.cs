d56bb0c469ee5b31e8987d3dbdf7fe47415fe9db
TramwajPL <56363425+TramwajPL@users.noreply.github.com>
2021-02-12 14:37:02 +0100

    added some new levels

--- CyclingMobile/Assets/Scripts/LevelCompleted.cs
+++ CyclingMobile/Assets/Scripts/LevelCompleted.cs
@@ -6,8 +6,14 @@ public class LevelCompleted : MonoBehaviour
 {
     public bool grass1;
     public bool grass2;
+    public bool grass3;
+    public bool grass4;
     public bool asphalt1;
     public bool asphalt2;
+    public bool asphalt3;
+    public bool asphalt4;
     public bool sand1;
     public bool sand2;
+    public bool sand3;
+    public bool sand4;
 }
